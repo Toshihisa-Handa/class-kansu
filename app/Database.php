7be01b4081e39eefc78603c6c9532e5add743327
Toshi <waykiyoharu@gmail.com>
2021-02-23 18:45:31 +0900

    db接続selfを外した

--- app/Database.php
+++ app/Database.php
@@ -2,19 +2,16 @@
 
 class Database
 {
-   private static $instance;
     public static function dbcon(){
 
-        //self::はstaticの時に使用する
-if(!isset(self::$instance)){
     try {
-        self::$instance = new PDO('mysql:host=localhost;dbname=dot_todos', 'root', 'root');
+        $instance = new PDO('mysql:host=localhost;dbname=dot_todos', 'root', 'root');
     } catch (PDOException $e) {
         print "エラー：" . $e->getMessage() . "<br/>";
         exit();
     }
-    return self::$instance;
-}
+    return $instance;
+
 
 }
        
