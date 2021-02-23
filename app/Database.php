1c989348ce6e5be9763bc8d6a3ccfdcbb6cd7adc
Toshi <waykiyoharu@gmail.com>
2021-02-23 18:46:01 +0900

    selfを付け直した

--- app/Database.php
+++ app/Database.php
@@ -2,16 +2,19 @@
 
 class Database
 {
+   private static $instance;
     public static function dbcon(){
 
+        //self::はstaticの時に使用する
+if(!isset(self::$instance)){
     try {
-        $instance = new PDO('mysql:host=localhost;dbname=dot_todos', 'root', 'root');
+        self::$instance = new PDO('mysql:host=localhost;dbname=dot_todos', 'root', 'root');
     } catch (PDOException $e) {
         print "エラー：" . $e->getMessage() . "<br/>";
         exit();
     }
-    return $instance;
-
+    return self::$instance;
+}
 
 }
        
